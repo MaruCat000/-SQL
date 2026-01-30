1d596006fb7b4265a1d71c4add0e1011a78768b3
center <t.aoyaama@gmail.com>
2026-01-30 14:57:57 +0900

    コメント追加

--- mondai2.sql
+++ mondai2.sql
@@ -1,3 +1,8 @@
+--問題 2
+--問題: 「John Doe」が投稿したツイートをすべて取得してください。
+--説明: usersテーブルとtweetsテーブルを結合し、first_nameが”John”で、last_nameが”Doe”のユー
+--ザーのツイートを取得します。
+
 SELECT tweet FROM tweets
 INNER JOIN users
 ON tweets.user_id = users.id

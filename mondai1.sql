2a206e1c43f0a039ea59b4374cc4bae9828f9e0e
center <t.aoyaama@gmail.com>
2026-01-30 14:54:03 +0900

    コメント追加

--- mondai1.sql
+++ mondai1.sql
@@ -1,1 +1,5 @@
-SELECT `first_name`, `last_name` FROM `users`;
+--問題 1
+--問題: 全てのユーザーの名前と姓を取得してください。
+--説明: usersテーブルのfirst_nameとlast_nameのすべてのレコードを取得するクエリを作成します。
+
+SELECT `first_name`, `last_name` FROM `users`;
